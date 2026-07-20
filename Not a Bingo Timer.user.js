675042da55d1b6acf6b6cf10725085c6241c63eb
𝕯𝖟𝖛𝖊𝖗𝖗𝖞 <74427436+Dzverry@users.noreply.github.com>
2026-07-20 23:13:45 +0300

    Update Not a Bingo Timer.user.js
    Fix for new batch of hosted sites

--- Not a Bingo Timer.user.js
+++ Not a Bingo Timer.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Not a Bingo Timer
 // @namespace    http://tampermonkey.net/
-// @version      2026-07-20
+// @version      2026-07-21
 // @updateURL    https://raw.githubusercontent.com/Dzverry/Not-a-Bingo-Timer/refs/heads/main/Not%20a%20Bingo%20Timer.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Dzverry/Not-a-Bingo-Timer/refs/heads/main/Not%20a%20Bingo%20Timer.user.js
 // @description  I mean, it's a timer... I wonder what it could do?? 🤔
 // @author       Dzverry
-// @match        https://bingopp-com-flash.bfcdl.com/bingo/web/*/app/chat_moderator/index.html
-// @match        https://www.zebrabingo.com/bingo/
-// @match        https://www.jazzycat.com/bingo/
+// @match        https://bingopp-com-flash.bfcdl.com/bingo/web/*/app/chat_moderator/index.htmlhtml
+// @match        https://casushi-uk.onegameslink.com/bingo/web/*/*/app/chat_moderator/index.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bingo.paddypower.com
 // @grant        none
 // ==/UserScript==
